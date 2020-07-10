fcb09c8448c5c9c4f9be124bc8e7a77d67b278e0
xariveros <xariveros@uc.cl>
2020-07-10 04:13:14 -0400

    Updates

---
+++ precache-manifest.3188521b6e582cad3e1cdc30b02ecc21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d51a85a19a8805889ad4aebfa4835f",
+    "url": "/t5/index.html"
+  },
+  {
+    "revision": "7e2afd9d1cba203693b2",
+    "url": "/t5/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "78c3fd879fcb83fc8a7d",
+    "url": "/t5/static/js/2.8e6a4056.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/t5/static/js/2.8e6a4056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e2afd9d1cba203693b2",
+    "url": "/t5/static/js/main.feca9d2b.chunk.js"
+  },
+  {
+    "revision": "1b4ebdb37bc25a704d89",
+    "url": "/t5/static/js/runtime-main.39af3e62.js"
+  }
+]);
